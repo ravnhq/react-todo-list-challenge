7db52282887a20291f36b469c656e2dc3dd55092
Alexander Apaza <aapaza@inkastudios.com>
2023-04-14 16:44:40 -0500

    feat: save tasks on localstorage

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,61 @@ function action(x) {
   console.log("Action: ", x);
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("tasks") === null) {
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
+});
+
+const renderTasksOnElementId = (tasks, elementId) => {
+  const taskContainerElement = document.getElementById(elementId);
+  const tasksHTML = tasks.reduce(
+    (acc, curr, idx) =>
+      acc +
+      `
+  <div class="task ${curr.priority}-priority">
+    <div class="task-title">
+      <h3>${curr.title}</h3>
+    </div>
+    <div class="task-content">
+      <span
+        >${curr.description}</span
+      >
+    </div>
+    <div class="task-footer">
+      <div class="task-action tooltip" onclick="action('edit')">
+        ✏️
+      </div>
+      <div class="vl"></div>
+      <div class="task-action tooltip" onclick="action('delete')">
+        ✅
+      </div>
+    </div>
+  </div>
+  `,
+    ""
+  );
+  console.log(tasksHTML)
+  taskContainerElement.innerHTML = tasksHTML;
+};
+
 document
   .getElementById("task-create-form")
   .addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event);
+    let formElements = event.target.elements;
+
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    let task = {
+      title: formElements["task-title"].value,
+      description: formElements["task-description"].value,
+      priority: formElements["task-priority"].value,
+    };
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+    renderTasksOnElementId(tasks, "tasks-container");
   });
